c6697308e4dd259145e53fc5fcabbefdb36e6c42
ilyas <92607100+uncognic@users.noreply.github.com>
2024-11-22 17:39:34 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -48,7 +48,11 @@ sudo chsh -s /usr/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp -i .zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp -i .zshrc ~/.zshrc 
+
+cp -i .p10k.zsh ~/.p10k.zsh
 
 
 
